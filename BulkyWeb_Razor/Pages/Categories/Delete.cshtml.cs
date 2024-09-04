04286f94e3456ec9be352f0c0508137f62685a0e
g4uravkh4 <gauravkhatiwada4@gmail.com>
2024-09-04 22:10:39 +0545

    add on razor

---
+++ BulkyWeb_Razor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,21 @@
+using BulkyWeb_Razor.Data;
+using BulkyWeb_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWeb_Razor.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BulkyWeb_Razor/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,21 @@
+using BulkyWeb_Razor.Data;
+using BulkyWeb_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWeb_Razor.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
